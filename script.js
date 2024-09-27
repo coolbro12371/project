aa554782975e209b40853696f30e8728b209f91a
Caleb <82981948+coolbro12371@users.noreply.github.com>
2024-09-27 13:28:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+// Script.js
+// Correct Pin Value
+let correctPin = "1234";
+
+let btns =
+    document.getElementsByClassName(
+        "pinpad-btn"
+    );
+let pinInput = document.getElementById(
+    "pinpad-input"
+);
+
+for (let i = 0; i < btns.length; i++) {
+    let btn = btns.item(i);
+    if (
+        btn.id &&
+        (btn.id === "submit-btn" ||
+            btn.id === "delete-btn")
+    )
+        continue;
+
+    // Add onclick event listener to 
+    // Every button from 0 - 9
+    btn.addEventListener(
+        "click",
+        (e) => {
+            pinInput.value +=
+                e.target.value;
+        }
+    );
+}
+
+let submitBtn = document.getElementById(
+    "submit-btn"
+);
+let delBtn = document.getElementById(
+    "delete-btn"
+);
+let modal =
+    document.getElementById("modal");
+let result =
+    document.getElementById("result");
+let closeBtn =
+    document.getElementById("close");
+
+submitBtn.addEventListener(
+    "click",
+    () => {
+        if (
+            !pinInput ||
+            !pinInput.value ||
+            pinInput.value === ""
+        ) {
+            alert(
+                "Please enter the arming code first!"
+            );
+        } else if (
+            pinInput.value ===
+            correctPin
+        ) {
+            alert("Correct Arming Code");
+            setTimeout(1000);
+            alert("Planting...");
+            document.getElementById('plantingsound').play();
+            setTimeout(3200);
+            document.getElementByID('planted').play();
+            setTimeout(40000);
+        } else {
+            alert("Incorrect PIN");
+        }
+        // Reset the input
+        pinInput.value = "";
+    }
+);
+
+delBtn.addEventListener("click", () => {
+    if (pinInput.value)
+        pinInput.value =
+            pinInput.value.substr(
+                0,
+                pinInput.value.length -
+                    1
+            );
+});
+
+closeBtn.addEventListener(
+    "click",
+    () => {
+        modal.style.display = "none";
+    }
+);
