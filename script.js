bdb03bcac7da439a380a5e2abdbd7fa98b24a826
Caleb <82981948+coolbro12371@users.noreply.github.com>
2024-09-30 16:21:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -64,11 +64,16 @@ submitBtn.addEventListener(
                 plantingaudio.play();
                 alert("Planting...");
                 setTimeout(audio2, 3200);
+                setTimeout(audio3, 40000);
             }
             setTimeout(audio1, 1000);
             function audio2() {
                 var plantedaudio = new Audio('sounds/planted2.mp3');
                 plantedaudio.play();
+            }
+            function audio3() {
+                var explodedaudio = new Audio('sounds/exploded3.mp3');
+                explodedaudio.play();
             }
         } else {
             alert("Incorrect PIN");
